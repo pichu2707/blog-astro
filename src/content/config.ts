2c02bb3a5d7441c5bac13fc458f4d17adc439f23
Pichu <pichu_2707@hotmail.com>
2025-03-12 00:45:42 +0100

    Subiendo ejercicio acabado, faltaría solucionar diseños con Tailwind

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z} from 'astro:content'
+import { defineCollection, reference, z} from 'astro:content'
 
 const blogCollection = defineCollection({
     type: 'content',
@@ -9,12 +9,30 @@ const blogCollection = defineCollection({
         description: z.string(),
         image: image(),
         //Relación, 
-        author : z.string(),
+        // author : z.string(),
+        author: reference('author'),
         //Relación
         tags: z.array(z.string()),
+
+        //Boolean
+        isDraft: z.boolean().default(false),
 }),
 });
 
+const authorCollection = defineCollection({
+    type: 'data',
+    schema:({image}) => z.object ({
+        name: z.string(),
+        avatar: image(),
+        twitter: z.string(),
+        linkedIn: z.string(),
+        github: z.string(),
+        bio: z.string(),
+        subtitle: z.string(),
+    }),
+})
+
 export const collections = {
     blog: blogCollection,
+    author: authorCollection,
 };
